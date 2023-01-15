92622d610d8088ff891d82df1f2c3729282aee30
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-01-15 15:23:48 +0800

    Add validation and 'api' prefix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,13 +10,28 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
 
+  // Append api prefix to your base url
+  app.setGlobalPrefix('api');
+  // Enable versioning on this api
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Remove and return error if unwanted properties exists
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+      // Automatically ransform payloads to be objects typed according to their DTO classes
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
